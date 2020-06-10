afa183026a753e6301295898cebcdd837b569ea7
清风一缕 <13452903663@163.com>
2020-06-10 13:44:20 +0800

    scheduler

--- src/index.js
+++ src/index.js
@@ -81,25 +81,118 @@
 
 // import {interval} from 'rxjs'
 
-
 // console.log(interval);
 
 // let observable = interval(400)
 
-import { from, Subject } from 'rxjs';
-import { multicast } from 'rxjs/operators';
- 
-const source = from([1, 2, 3]);
-const subject = new Subject();
-const multicasted = source.pipe(multicast(subject));
- 
-// These are, under the hood, `subject.subscribe({...})`:
-multicasted.subscribe({
-  next: (v) => console.log(`observerA: ${v}`)
+// import { from, Subject } from 'rxjs';
+// import { multicast } from 'rxjs/operators';
+
+// const source = from([1, 2, 3]);
+// const subject = new Subject();
+// const multicasted = source.pipe(multicast(subject));
+
+// // These are, under the hood, `subject.subscribe({...})`:
+// multicasted.subscribe({
+//   next: (v) => console.log(`observerA: ${v}`)
+// });
+// multicasted.subscribe({
+//   next: (v) => console.log(`observerB: ${v}`)
+// });
+
+// // This is, under the hood, `source.subscribe(subject)`:
+// multicasted.connect();
+
+/* behaviorSubject 行为主体 */
+
+/* import { Subject, BehaviorSubject } from 'rxjs';
+
+const bsubject = new BehaviorSubject(0);
+bsubject.subscribe({
+    next: v => console.log(`bobsA: ${v}`)
+})
+bsubject.next(1)
+bsubject.next(2)
+bsubject.subscribe({
+    next: v => console.log(`bobsB: ${v}`)
+})
+bsubject.next(3)
+console.log("--------");
+const subject = new Subject(0);
+subject.subscribe({
+    next: v => console.log(`obsA: ${v}`)
+})
+subject.next(1)
+subject.next(2)
+subject.subscribe({
+    next: v => console.log(`obsB: ${v}`)
+})
+subject.next(3) */
+
+/* ReplaySubject 旧值推送，参数：推送历史条数 */
+
+// import { ReplaySubject } from 'rxjs';
+// const subject = new ReplaySubject(3);
+
+// subject.subscribe({
+//   next: (v) => console.log(v),
+// });
+
+// subject.next(1);
+// subject.next(2);
+// subject.next(3);
+// subject.next(4);
+
+// subject.subscribe({
+//   next: (v) => console.log('two' + v),
+// });
+// subject.next(5);
+
+/* 同步主体，只有在完成时才会发送最后一个值给观察者 */
+
+// import { AsyncSubject } from 'rxjs';
+// const subject = new AsyncSubject();
+
+// subject.subscribe({
+//   next: (v) => console.log(v),
+// });
+
+// subject.next(1);
+// subject.next(2);
+// subject.next(3);
+// subject.next(4);
+
+// subject.subscribe({
+//   next: (v) => console.log(`observerB: ${v}`),
+// });
+
+// subject.next(5);
+// subject.complete();
+
+/* Scheduler 调度器 */
+
+import { Observable, asyncScheduler, from } from 'rxjs';
+import { observeOn } from 'rxjs/operators';
+
+const observable = new Observable((observer) => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.complete();
+}).pipe(observeOn(asyncScheduler));
+
+console.log('just before subscribe');
+
+observable.subscribe({
+  next(x) {
+    console.log('got value ' + x);
+  },
+  error(err) {
+    console.error('something wrong occurred: ' + err);
+  },
+  complete() {
+    console.log('done');
+  },
 });
-multicasted.subscribe({
-  next: (v) => console.log(`observerB: ${v}`)
-});
- 
-// This is, under the hood, `source.subscribe(subject)`:
-multicasted.connect();
+
+console.log('just after subscribe');
